dd0239b282edb4aceab9975f11abaa44ab83a456
Yee Y <s0m3y3@gmail.com>
2023-09-05 23:41:05 -0500

    added back .env file and included it with gitignore. Tidy some other code and/or deleted unnecessary code/comments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //this is main system of Employee Tracker. 
 const inquirer = require("inquirer");
 const {menuInput} = require('./lib/menuItem.js');
+require('dotenv').config();
 
 const menuItems = [
     "Add Employee",
@@ -24,13 +25,10 @@ const questions = [
 
 //function to initialize app
 async function init() {
-
     const data = await inquirer.prompt(questions);
-    // console.log("init is working: ", data);
     if (data.text==="Quit"){return;};  //exit function, if Quit is selected. 
-    await menuInput(data.text);  //input prompt into menuInput
+    await menuInput(data.text);  //input prompt into menuInput, which is the logic of the code. 
     setTimeout(init, 500);   
-    // await init(); 
 }
 
 init(); 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const PORT = 3001;
 const app = express();
@@ -10,49 +11,19 @@ app.use(express.json());
 const db = mysql.createConnection(
   {
     host: 'localhost', 
-    user: 'root', //default is "root". Please update, to your configuration.
-    password: '', //default is "". Please update, to your configuration.
-    database: 'employeeTracker_db' //The program currently only have one database, as listed. 
+    user: process.env.DB_USER, //default is "root". Please update, to your configuration.
+    password: process.env.DB_PASSWORD, //default is "". Please update, to your configuration.
+    database: process.env.DB_NAME, //The program currently only have one database, as listed. 
   },
   console.log(`Connected to the employeeTracker_db database.`)
 );
 
-let sqlQuery ="";
-// function performDatabaseQueries(queryType, callback) {
-//   db;
-
-//   // Query to select department names
-//   if (queryType === 'viewAllDepartments') { sqlQuery = 'SELECT name FROM Department';} 
-//   else if (queryType === 'viewAllRoles') { sqlQuery = 'SELECT title FROM Role'
-//     // pending code.
-//   }
-//   //pending more. 
-
-//   if (sqlQuery) {
-//     db.query(sqlQuery, (error, results) => {
-//       if (error) throw error;
-  
-//       console.log('Query result:', results);
-  
-//       // You can process and display the results as needed
-//       // For example, results may be an array of department names
-//       results.forEach(row => {
-//         console.log(`Department Name: ${row.name}`);
-//       });
-  
-//       connection.end();
-//     });
-//   } else {
-//     console.log('Invalid query type.');
-//     connection.end();
-//   };
-// };
-
 function performDatabaseQueries(queryType, callback) {
-  let sqlQuery = '';
+
+  let sqlQuery = ''; //this is the query key word search, based on query type. For example: 'SELECT * FROM Department'
 
   if (queryType === 'viewAllDepartments') {
-    console.log('\n Department Names: \n');
+    console.log('\n Department Names: \n');  //adds title to the table. 
     sqlQuery = 'SELECT * FROM Department';
   }
   // Add other query types as needed
@@ -60,8 +31,7 @@ function performDatabaseQueries(queryType, callback) {
   if (sqlQuery) {
     db.query(sqlQuery, (error, results) => {
       if (error) throw error;
-      // Call the callback function with the results
-      console.table(results);  //this is to remove the index
+      console.table(results);  //display table.
     });
   } else {
     console.log('Invalid query type.');
